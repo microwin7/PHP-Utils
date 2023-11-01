780aecb7d5bda18c040987446c52577d1e4b45e3
microwin7 <usa.microwin8@gmail.com>
2023-11-01 08:03:37 +0300

    Added RequiredArguments in construct

--- src/Exceptions/RequiredArgumentMissingException.php
+++ src/Exceptions/RequiredArgumentMissingException.php
@@ -4,7 +4,8 @@ namespace Microwin7\PHPUtils\Exceptions;
 
 class RequiredArgumentMissingException extends \Exception
 {
-    function __construct() {
-        parent::__construct("Отсутствуют обязательные аргументы");
+    function __construct(string|array $arguments = 'ENCRYPTED')
+    {
+        parent::__construct("Отсутствуют обязательные аргументы: " . is_array($arguments) ? implode(', ', $arguments) : $arguments);
     }
 }

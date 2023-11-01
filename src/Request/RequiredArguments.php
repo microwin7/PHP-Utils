778c359c1b4a3744c0c299841711600767e89510
microwin7 <usa.microwin8@gmail.com>
2023-11-01 11:25:32 +0300

    Added experimental class RequiredArguments

---
+++ src/Request/RequiredArguments.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Microwin7\PHPUtils\Request;
+
+use Microwin7\PHPUtils\Exceptions\RequiredArgumentMissingException;
+
+class RequiredArguments
+{
+    private array $arguments;
+    public function __construct()
+    {
+    }
+    public function __get($name): string|array
+    {
+        return $this->arguments[$name];
+    }
+    public static function validate(array $requiredArguments, ?array $where): static
+    {
+        if ((!empty($requiredArguments) && !empty($where)) && count($requiredArguments) > count($where)) {
+            $class = new static();
+            foreach ($requiredArguments as $argument) {
+                $class->with($argument, $where[$argument] ?? new RequiredArgumentMissingException($requiredArguments));
+            }
+        } else throw new RequiredArgumentMissingException($requiredArguments);
+        return $class;
+    }
+    private function with(string $property, string|array $value): void
+    {
+        $this->arguments[$property] = $value;
+    }
+}

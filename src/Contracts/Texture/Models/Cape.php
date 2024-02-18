c0880a775f8f85a8f4587831d2c459bc1c1a9df8
microwin7 <usa.microwin8@gmail.com>
2024-02-19 02:18:36 +0300

    Fix import

--- src/Contracts/Texture/Models/Cape.php
+++ src/Contracts/Texture/Models/Cape.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Microwin7\PHPUtils\Contracts\Texture\Models;
 
 use Microwin7\PHPUtils\Utils\Texture;
+use Microwin7\PHPUtils\Configs\TextureConfig;
 use Microwin7\PHPUtils\Contracts\Texture\Enum\TextureStorageTypeEnum;
 
 abstract class Cape implements \JsonSerializable
@@ -16,8 +17,8 @@ abstract class Cape implements \JsonSerializable
         public readonly TextureStorageTypeEnum          $textureStorageType,
         public readonly string                          $data,
         public readonly string                          $url,
-                        string|null                     $digest = null,
+        string|null                     $digest = null,
     ) {
-        $this->digest = null !== $digest ? $digest : (TextureConfig::LEGACY_DIGEST ? Texture::digest_legacy($this->data) :Texture::digest($this->data));
+        $this->digest = null !== $digest ? $digest : (TextureConfig::LEGACY_DIGEST ? Texture::digest_legacy($this->data) : Texture::digest($this->data));
     }
 }

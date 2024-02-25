c9b866850fd8ee2d68055df2f4ad3e149644604e
microwin7 <usa.microwin8@gmail.com>
2024-02-25 22:56:27 +0300

    Update Regex rules and combineOR for regex's patterns method added

--- src/Rules/Regex.php
+++ src/Rules/Regex.php
@@ -9,22 +9,25 @@ class Regex
     public const DISALLOW_FIRST_SPACE = '/^\s/';
     public const DISALLOW_LAST_SPACE = '/\s$/';
     public const ID_REGXP = '/^[a-zA-Z0-9\-\_\:\.]+$/';
-    public const DISPLAYNAME_REGXP = '/^[a-zA-Zа-яА-ЯЁё0-9\-\_\ \(\)\[\]\.\,\"\«\»\/]+$/u';
+    public const DISPLAYNAME_REGXP = '/^[a-zA-Zа-яА-ЯЁё0-9\-\_\ \(\)\[\]\.\,\"\«\»\/]+$/';
     public const NUMERIC_REGXP = '/^[0-9]+$/';
     public const CATEGORY_REGXP = '/^[A-Z0-9\_]+$/';
     public const ITEM_LINK_REGXP = '/^[a-zA-Z0-9\_\-\.]+$/';
-    public const SERVER_REGXP = '/^([a-zA-Z]+)*$/';
-    public const LOGIN = "/^[a-zA-Z0-9_-]{1,64}$/";
-    public const USERNAME = "/^\w{2,16}$/";
-    public const UUID_NO_DASH = "/^[0-9a-f]{32}$/";
-    public const UUIDv1_AND_v4 = "/^[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}$/";
+    public const SERVER_REGXP = '/^[a-zA-Z]+$/';
+    public const LOGIN = '/^[a-zA-Z0-9_-]{1,64}$/';
+    public const USERNAME = '/^\w{2,16}$/';
+    public const UUID_NO_DASH = '/^[0-9a-f]{32}$/';
+    public const UUIDv1_AND_v4 = '/^[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}$/';
     public const MD5 = self::UUID_NO_DASH;
-    public const SHA1 = "/^[0-9a-f]{40}$/";
-    public const SHA256 = "/^[0-9a-f]{64}$/";
+    public const SHA1 = '/^[0-9a-f]{40}$/';
+    public const SHA256 = '/^[0-9a-f]{64}$/';
 
+    /**
+     * @throws \InvalidArgumentException
+     */
     public static function valid_with_pattern(string $string, string $pattern): bool
     {
-        return empty($pattern) ? false : (bool)preg_match($pattern, $string, $varR);
+        return empty($pattern) ? throw new \InvalidArgumentException($pattern) : (bool)preg_match($pattern, $string, $varR);
     }
     public static function valid_username(string $string): bool
     {
@@ -41,5 +44,12 @@ class Regex
     public static function contains(string $haystack, string $needle): bool
     {
         return strpos($haystack, $needle) !== false;
+    }
+    public static function combineOR(string ...$patternArray): string
+    {
+        $combinedPattern = implode('|', array_map(function ($pattern) {
+            return rtrim(ltrim($pattern, '/'), '/');
+        }, $patternArray));
+        return '/' . $combinedPattern . '/';
     }
 }
